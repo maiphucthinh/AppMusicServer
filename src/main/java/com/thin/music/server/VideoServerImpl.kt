e7244183574a181a77cb0ffc690291f14e3d0601
Thịnh Mai <thinh@gmail.com>
2020-10-02 12:07:41 +0700

    update

--- src/main/java/com/thin/music/server/VideoServerImpl.kt
+++ src/main/java/com/thin/music/server/VideoServerImpl.kt
@@ -11,23 +11,29 @@ import java.io.IOException
 
 @Service
 class VideoServerImpl : VideoServer {
+    val listVideoNews: MutableList<ItemVideoOnline> = ArrayList()
     override fun getVideo(): Any? {
-        val onlines: MutableList<ItemVideoOnline> = ArrayList()
+        getVideoNewsByLink("https://chiasenhac.vn/video-moi.html")
+        getVideoNewsByLink("https://chiasenhac.vn/video-moi.html?page=2")
+        getVideoNewsByLink("https://chiasenhac.vn/video-moi.html?page=3")
+        getVideoNewsByLink("https://chiasenhac.vn/video-moi.html?page=4")
+        return listVideoNews
+    }
+    private fun getVideoNewsByLink(link:String){
         try {
-            val doc = Jsoup.connect("https://chiasenhac.vn/video-moi.html").get()
+            val doc = Jsoup.connect(link).get()
             val els = doc.select("div.content-wrap").select("div.col")
-                for (child in els) {
-                    val linkImage = child.select("div.card-header").attr("style").replace("background-image: url(", "").replace(");", "")
-                    val linkVideo = child.select("a").attr("href")
-                    val nameVideo = child.select("a").attr("title")
-                    val artistName = child.select("p").select("a").text()
+            for (child in els) {
+                val linkImage = child.select("div.card-header").attr("style").replace("background-image: url(", "").replace(");", "")
+                val linkVideo = child.select("a").attr("href")
+                val nameVideo = child.select("a").attr("title")
+                val artistName = child.select("p").select("a").text()
 
-                    onlines.add(ItemVideoOnline(null, linkImage, linkVideo, nameVideo, artistName))
-                }
+                listVideoNews.add(ItemVideoOnline(null, linkImage, linkVideo, nameVideo, artistName))
+            }
         } catch (e: IOException) {
             e.printStackTrace()
         }
-        return onlines
     }
 
     override fun getLinkVideo(linkVideo: String): Any {
@@ -36,16 +42,10 @@ class VideoServerImpl : VideoServer {
                         .get()
         val els = c.select("div.col-12").select("li")
         val childEls = els.select("li")
-       return if (childEls.size >= 2) {
-           GetLinkMusic(
-                   childEls.get(1).select("a").attr("href")
-           )
-//            val linkVideo = childEls.get(1).select("a").attr("href")
+        return if (childEls.size >= 2) {
+            GetLinkMusic(childEls.get(1).select("a").attr("href"))
         } else {
-           GetLinkMusic(
-                   childEls.get(0).select("a").attr("href")
-           )
-//            val linkVideo1 = childEls.get(0).select("a").attr("href")
+            GetLinkMusic(childEls.get(0).select("a").attr("href"))
         }
     }
 }
